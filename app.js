256b289b18678423958384d7667201d7c5eb39b2
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-06-24 07:47:49 +0530

    End of section 3

--- app.js
+++ app.js
@@ -5,51 +5,15 @@
 
 //importing http lib
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 //for importing a local module we need to give ./ before the file name
 //for core modules it can be ignored
 
 
 
-function reqListener(req, res){
-    const url = req.url ;
-    const method = req.method;
-    if(url =='/'){
-        res.setHeader('Content-type','text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit"> Send </button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    
-    if(url ==='/message' && method === 'POST'){
-        const body = [];
-        //on is an even listerner and it takes two arguments 
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
-
-    }
-    res.setHeader('Content-type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My Page/title></head>');
-    res.write('<body><h1> Hello from my Node.js server</h1></body>');
-    res.write('</html');
-    res.end();
-}
 //creating a server method 1
-const server = http.createServer(reqListener);
+const server = http.createServer(routes.handler);
 
 
 server.listen(3000);
