d4dd9546dcbc3fa68f46e4afcbc317f781e86556
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-07-03 08:06:04 +0530

    End of HTML Routing

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express  = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Another Middleware');
-    res.send('<h1>Hello from lol</h1>');
+app.use(bodyParser.urlencoded());
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res , next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
-
-app.use('/', (req, res, next) => {
-    console.log('Another Middleware');
-    res.send('<h1>Hello from express</h1>');
-});
-
 
 app.listen(3000);
