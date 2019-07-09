46d836459bd74966ef4e4daaf05d01c4f17aa3f2
admin <sbk888.sbk@gmail.com>
2019-07-09 12:36:36 +0530

    End of the PUG

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const products =[];
 router.get('/add-product', (req, res, next) => {
     //res.sendFile(path.join(__dirname,'../','views','add-product.html'));
     //res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('add-product',{pageTitle : 'Add Product'});
+    res.render('add-product',{pageTitle : 'Add Product', path:'/admin/add-product'});
 });
 
 router.post('/add-product',(req, res, next) =>{
