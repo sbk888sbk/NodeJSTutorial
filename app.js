f7a7de5548ad45c65bb739d24e94b28e7c6c5d69
admin <sbk888.sbk@gmail.com>
2019-07-09 18:13:03 +0530

    HBS

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 
 const app = express();
-app.set('view engine','pug');
+// To use PUG
+// app.set('view engine','pug');
+// app.set('views','views');
+//PUg Has inbuilt engine in express
+
+
+
+//To speicfy handbars is installed and we have to use engine
+app.engine('hbs', expressHbs());
+app.set('view engine','hbs');
 app.set('views','views');
 
 const path = require('path');
@@ -25,7 +35,7 @@ app.use('/',shopRoutes);
 
 app.use((req, res , next) => {
     //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404', {pageTitle : "Page Not Found"})
+    res.status(404).render('404', {pageTitle : "Page Not Found",layout: false})
 });
 
 app.listen(3000);
