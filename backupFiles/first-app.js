054ad50c49ac0248126c02bec19b5415a50ac887
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-07-13 13:29:58 +0530

    Cleaning up after templating engines

--- backupFiles/first-app.js
+++
@@ -1,7 +0,0 @@
-//We use " require " keyword to import a lib
-const fs = require('fs');
-
-console.log("Hello World");
-
-//writeFile function take 2 arguments 1. path  2. Contnet to write to file. 
-fs.writeFile("hi.txt", 'Hello from the node js');
