464bde7757d3537fe1c142a455b9d55a04250818
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-07-13 13:31:11 +0530

    deleted assignments folder | All present in master

--- assignment/Assignment_3/util/path.js
+++
@@ -1,4 +0,0 @@
-//This is a custom module path(different from exported module)
-//Usint this path to define root folder of our app which is exported and is used in other files
-const path = require('path');
-module.exports = path.dirname(process.mainModule.filename);
