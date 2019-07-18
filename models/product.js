08d9f0d542d169d9f738c21bec7ea17e5b0a4ef8
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-07-18 08:28:04 +0530

    End of MVC

--- models/product.js
+++ models/product.js
@@ -1,14 +1,40 @@
-const products = [];
+let products = [];
+
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename),
+'data',
+'products.json');
+
+
+const getProductsFromFile = cb => {
+
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }else {
+        return cb(JSON.parse(fileContent));
+        }
+    });
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
-    save(){
-        products.push(this);
+    save() {
+        getProductsFromFile( products => {
+            products.push(this);
+            
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+        })
+
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
