ce163c10efd4baa525e745d9157931a67d72c935
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-07-21 08:29:55 +0530

    Advanced Routing

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product')
 exports.getAddProduct = (req, res, next) => {
     //res.sendFile(path.join(__dirname,'../','views','add-product.html'));
     //res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         productCSS: true,
@@ -20,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     const products = Product.fetchAll((products) => {
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
