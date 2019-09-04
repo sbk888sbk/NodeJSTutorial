448df628f96f799101f5dd6b4ec97827b63fc301
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-09-04 08:53:06 +0530

    Completed Query Parameters

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,11 +3,10 @@ const Product = require('../models/product')
 exports.getAddProduct = (req, res, next) => {
     //res.sendFile(path.join(__dirname,'../','views','add-product.html'));
     //res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        productCSS: true,
-        activeAddProduct: true
+
     });
 };
 
@@ -20,6 +19,18 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(editMode!=='true'){
+        return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '',
+        editing : editMode
+    });
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -10,11 +10,12 @@ const adminController = require('../controllers/admin')
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => POST
-router.post('/add-product',adminController.postAddProduct);
-
 // /admin/products => GET
 router.get('/products',adminController.getProducts);
 
+// /admin/add-product => POST
+router.post('/add-product',adminController.postAddProduct);
+
+router.get('/edit-product/:productId',adminController.getEditProduct);
 
 module.exports= router;
